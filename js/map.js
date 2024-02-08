a44782102c714193fd7b3b123da19c07305d1364
shtrotter <samueltrotter1@gmail.com>
2024-02-07 19:06:51 -0500

    Vietnam update

--- js/map.js
+++ js/map.js
@@ -9,10 +9,19 @@ var marker = L.marker([39.74257870459885, -104.9340878016765]).addTo(map)
 		.openPopup();
 		
 var marker = L.marker([48.87169887112109, 2.3456590673877202]).addTo(map)
-		.bindPopup('<b>Paris, FR</b> <br> June Trip with Mom, Cassie, and Sam');
+		.bindPopup('<b>Paris, Francew</b> <br> June Trip with Mom, Cassie, and Sam');
 
-var marker = L.marker([35.32234477374835, 24.544360080351105]).addTo(map)
-		.bindPopup('<b>Paris, FR</b> <br> June Trip with Mom, Cassie, and Sam');
+var marker = L.marker([43.29786538386166, 5.406695144698911]).addTo(map)
+		.bindPopup('<b>Marseille, France</b> <br> June Trip with Mom, Cassie, and Sam');
 		
 var marker = L.marker([21.031545954291136, 105.84583832159872]).addTo(map)
-		.bindPopup('<b>Vietnam, 1st AirBnB</b> <br> First Place We Stayed');
+		.bindPopup('<b>Hanoi, Vietnam</b> <br>1st AirBnB');
+
+var marker = L.marker([20.8024189381045, 106.99553079762494]).addTo(map)
+		.bindPopup('<b>Cat Ba, Vietnam</b>');
+
+var marker = L.marker([22.362196216085742, 103.80972503855087]).addTo(map)
+		.bindPopup('<b>Sa Pa, Vietnam</b>');
+		
+var marker = L.marker([28.64860307747513, 77.23193364636423]).addTo(map)
+		.bindPopup('<b>Delhi, India</b>');
